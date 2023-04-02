151ef7debf3933daeec0acced75e3a00c7cc5255
Dmitry_home <79811924115@yandex.ru>
2023-04-02 21:15:59 +0300

    Урок 12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="appWrapper">
+      <header className="header">
+        <div className="logo">
+          <img
+            src="https://yoomark.com/sites/default/files/field/image/react-technologies-9ce31d51432424bcd4626f5f730580f3cddc9c211c1577e9950138550233542a_0.png"
+            alt="logo"
+          />
+          <div>React reducs samury curs</div>
+        </div>
       </header>
+      <nav className="nav">
+        <div>
+          <a href="./#">Profile</a>
+        </div>
+        <div>
+          <a href="./#">Messages</a>
+        </div>
+        <div>
+          <a href="./#">Music</a>
+        </div>
+        <div>
+          <a href="./#">News</a>
+        </div>
+        <div>
+          <a href="./#">Settings</a>
+        </div>
+      </nav>
+      <main className="content">
+        <div className="mainBaner" />
+        <div>ava + discrription</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+            <div>Post3</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
