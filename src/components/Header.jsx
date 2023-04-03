295845096183c7eb186bf2de381c5c85cdd48ba2
Dmitry_home <79811924115@yandex.ru>
2023-04-03 10:42:59 +0300

    урок 14 (урок 13 пропускаю там про гит все без изменений в коде

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="header">
+        <div className="logo">
+          <img
+            src="https://yoomark.com/sites/default/files/field/image/react-technologies-9ce31d51432424bcd4626f5f730580f3cddc9c211c1577e9950138550233542a_0.png"
+            alt="logo"
+          />
+          <div>REACT REDUKS CURS</div>
+        </div>
+      </header>
+  )
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div className="content">
+      <div className="mainBaner" />
+      <div>ava + discrription</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post1</div>
+          <div>Post2</div>
+          <div>Post3</div>
+        </div>
+      </div>
+    </div>
+  );
+}
