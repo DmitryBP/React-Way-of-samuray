d29b5bc27d89c15c2b7f59b51772d9ffae4849ff
Dmitry_home <79811924115@yandex.ru>
2023-04-11 18:58:22 +0300

    Урок 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import s from './Dialogs.module.css'
+import React from 'react';
+import s from './Dialogs.module.css';
 
 export default function Dialogs() {
   return (
-    <div>
-      Messages
+    <div className={s.DialogsWrapper}>
+      <div className={s.dialogs}>
+        <div className={s.dialog}>Vika</div>
+        <div className={s.dialog}>Tania</div>
+        <div className={s.dialog}>Nina</div>
+        <div className={s.dialog}>Vladimir</div>
+        <div className={s.dialog}>Dima</div>
+      </div>
+      <div className={s.Messages}>
+        <div className={s.Message}>Hi</div>
+        <div className={s.Message}>How are you?</div>
+        <div className={s.Message}>Im fine</div>
+        <div className={s.Message}>And you?</div>
+      </div>
     </div>
-  )
+  );
 }
