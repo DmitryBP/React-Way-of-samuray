1bd3b219328a3bba07b14458667bda5fa45aa225
Dmitry_home <79811924115@yandex.ru>
2023-05-19 13:19:31 +0300

    Урок 61-1

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -5,20 +5,22 @@ import { connect } from 'react-redux';
 import { setUserProfile } from '../../Redux/Profile-reducer';
 import { useParams } from 'react-router-dom';
 
-const ProfileContainer = (props) => {
+function ProfileContainer(props) {
   let { userId } = useParams();
-  useEffect((props) => {
+    if (!userId) {
+        userId = 2;
+    }
+  useEffect(() => {
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then((respons) => {
       props.setUserProfile(respons.data);
     });
   }, [userId]);
-
   return (
     <div>
       <Profile {...props} profile={props.profile} />
     </div>
   );
-};
+}
 const mapStateToProps = (state) => ({
   profile: state.postPage.profile,
 });
